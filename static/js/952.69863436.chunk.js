f396e283b408f37024f40bd5406332a725ea9f48
tuxtoto <t.impery@nouveauxhorizons.org>
2025-04-13 11:10:16 +0200

    Updates

---
+++ static/js/952.69863436.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpolitical_leaderboard=self.webpackChunkpolitical_leaderboard||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.69863436.chunk.js.map
